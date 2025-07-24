99618b4e97d2c0bb09b3e5e42f9c4240e2bf64f6
bisunesskyu <bisunesskyu@gmail.com>
2025-07-24 22:32:04 +0900

    07/24 Home

--- CodingTest/src/step3/Main03.java
+++ CodingTest/src/step3/Main03.java
@@ -16,5 +16,12 @@ public class Main03 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
+		int sum = 0;
+		
+		for(int i = 1; i<=a; i++) {
+			sum += i;
+		}
+		
+		System.out.println(sum);
 	}
 }
